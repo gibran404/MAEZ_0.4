a1bcf9f98fa6b6492e82e8a5a1c8fd94e4d1b7cc
Muhammad Ali Momin <83720612+gibran404@users.noreply.github.com>
2024-10-08 17:26:36 +0500

    Revert "Navmeshes and Follow Enemies Implemented."
    This reverts commit d9af6827036987086ad94763a68fd3dfdd5e1b89.

---
+++ Assets/MAEZ/Scripts/Enemy Follow/Player.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private Animator Animator;
+    private Coroutine LookCoroutine;
+
+    [SerializeField]
+    private int Health = 300;
+
+    private const string ATTACK_TRIGGER = "Attack";
+
+    private IEnumerator LookAt(Transform Target)
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(Target.position - transform.position);
+        float time = 0;
+
+        while (time < 1)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, time);
+
+            time += Time.deltaTime * 2;
+            yield return null;
+        }
+
+        transform.rotation = lookRotation;
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        Health -= Damage;
+
+        if (Health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public Transform GetTransform()
+    {
+        return transform;
+    }
+}
--- Assets/Scripts/Enemy/EnemyState.cs
+++
@@ -1,7 +0,0 @@
-﻿public enum EnemyState
-{
-    Spawn,
-    Idle,
-    Patrol,
-    Chase
-}
